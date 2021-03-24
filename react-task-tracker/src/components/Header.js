2f241185c9def74563cc9b0f22ca59d8a1182793
Ridhan Fadhilah <ridhanfadhilah@gmail.com>
2021-03-24 17:53:07 +0700

    Header

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
-
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click');
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
@@ -16,5 +21,11 @@ Header.defaultProps = {
 Header.propTypes = {
   title: PropTypes.string,
 }
+
+// CSS in JS
+// const headingStyle = {
+//   color: 'red', 
+//   backgroundColor: 'black'
+// }
 
 export default Header
